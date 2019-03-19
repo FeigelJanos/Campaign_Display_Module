c2fe2844926aa51d9a3b0ce97a03fc0e5cadf484
John Feigel <32643682+FeigelJanos@users.noreply.github.com>
2019-03-19 09:58:05 +0100

    Create helper.php

---
+++ mod_campaigndisplay/helper.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Helper class for Campaign Display module
+ * 
+     * @package    Campign.Display
+     * @subpackage mod_campaigndisplay
+     * @copyright  Copyright (C) 2019 Money4You Inc. All rights reserved.
+     * @license    GNU/GPL, see LICENSE.php
+ * mod_campaigndisplay is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+//
+class ModCampaignDisplayHelper
+{
+    /**
+     * Retrieves the message
+     *
+     * @param   array  $params An object containing the module parameters
+     *
+     * @access public
+     */    
+    public static function getCampaign($params)
+    {
+        return 'Hello, World!';
+    }
+}
